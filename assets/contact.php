9b7a1f811439a05165e3459aa3cea1e7ae4c0e0e
Hyper Vare <135940595+hypervare@users.noreply.github.com>
2026-03-30 05:17:48 +0530

    Last Updated (30/03/2026 - 5:17 AM) by gunnu

--- assets/contact.php
+++ assets/contact.php
@@ -262,7 +262,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $mail = new PHPMailer(true);
     
-    $mailTO = "hypervare@gmail.com";
+    $mailTO = "info@ohmint.in";
     $sub = "OHMINT Website Enquiry";
     $fullname = $_POST['user_FirstName']." ".$_POST['user_LastName'];
     $email = $_POST['user_email'];
